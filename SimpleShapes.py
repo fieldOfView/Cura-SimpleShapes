eada285786bc8f667be1fe01c4b2b5d99f1bd386
fieldOfView <aldo@fieldofview.com>
2019-03-04 12:22:49 +0100

    Use trimesh primitives instead of rolling our own.

--- SimpleShapes.py
+++ SimpleShapes.py
@@ -1,15 +1,17 @@
-# Copyright (c) 2015 Ultimaker B.V.
-# Cura is released under the terms of the LGPLv3 or higher.
+# Copyright (c) 2018 fieldOfView
+# The SimpleShapes plugin is released under the terms of the AGPLv3 or higher.
 
 from PyQt5.QtCore import QObject
 
 import numpy
 import math
+import trimesh
 
 from UM.Extension import Extension
 from UM.Application import Application
 
-from UM.Mesh.MeshBuilder import MeshBuilder
+from UM.Mesh.MeshData import MeshData, calculateNormalsFromIndexedVertices
+
 from UM.Operations.AddSceneNodeOperation import AddSceneNodeOperation
 from cura.Scene.CuraSceneNode import CuraSceneNode
 from cura.Scene.SliceableObjectDecorator import SliceableObjectDecorator
@@ -19,6 +21,8 @@ from UM.i18n import i18nCatalog
 catalog = i18nCatalog("cura")
 
 class SimpleShapes(Extension, QObject,):
+    __size = 20
+
     def __init__(self, parent = None):
         QObject.__init__(self, parent)
         Extension.__init__(self)
@@ -30,116 +34,38 @@ class SimpleShapes(Extension, QObject,):
         self.addMenuItem(catalog.i18nc("@item:inmenu", "Add a sphere"), self.addSphere)
 
     def addCube(self):
-        mesh = MeshBuilder()
-
-        # Can't use MeshBuilder.addCube() because that does not get per-vertex normals
-        size = 20
-        minSize = -size / 2
-        maxSize = size / 2
-
-        verts = [ #4 corners in 6 faces.
-            [minSize, minSize, maxSize],
-            [minSize, maxSize, maxSize],
-            [maxSize, maxSize, maxSize],
-            [maxSize, minSize, maxSize],
-
-            [minSize, maxSize, minSize],
-            [minSize, minSize, minSize],
-            [maxSize, minSize, minSize],
-            [maxSize, maxSize, minSize],
-
-            [maxSize, minSize, minSize],
-            [minSize, minSize, minSize],
-            [minSize, minSize, maxSize],
-            [maxSize, minSize, maxSize],
-
-            [minSize, maxSize, minSize],
-            [maxSize, maxSize, minSize],
-            [maxSize, maxSize, maxSize],
-            [minSize, maxSize, maxSize],
-
-            [minSize, minSize, maxSize],
-            [minSize, minSize, minSize],
-            [minSize, maxSize, minSize],
-            [minSize, maxSize, maxSize],
-
-            [maxSize, minSize, minSize],
-            [maxSize, minSize, maxSize],
-            [maxSize, maxSize, maxSize],
-            [maxSize, maxSize, minSize],
-        ]
-        mesh.setVertices(numpy.asarray(verts, dtype=numpy.float32))
-
-        indices = [ #All 6 quads (12 triangles).
-            [0, 2, 1],
-            [0, 3, 2],
-
-            [4, 6, 5],
-            [4, 7, 6],
-
-            [8, 10, 9],
-            [8, 11, 10],
-
-            [12, 14, 13],
-            [12, 15, 14],
-
-            [16, 18, 17],
-            [16, 19, 18],
-
-            [20, 22, 21],
-            [20, 23, 22]
-        ]
-        mesh.setIndices(numpy.asarray(indices, dtype=numpy.int32))
-
-        mesh.calculateNormals()
-        self._addShape(mesh.build())
+        self._addShape(self._toMeshData(trimesh.primitives.Box(extents = [self.__size, self.__size, self.__size])))
 
     def addCylinder(self):
-        mesh = MeshBuilder()
-        # TODO: construct cylinder
-        self._addShape(mesh.build())
+        self._addShape(self._toMeshData(trimesh.primitives.Cylinder(radius = self.__size / 2, height = self.__size)))
 
     def addSphere(self):
-        mesh = MeshBuilder()
+        self._addShape(self._toMeshData(trimesh.primitives.Sphere(radius = self.__size / 2)))
 
-        radius = 10
-        resolution = 24
-
-        verts = []
-
-        for z_iteration in range(0, resolution):
-            z_value = math.pi * z_iteration / (resolution-1)
-            y = radius * math.cos(z_value)
-            local_radius = radius * math.sin(z_value)
-            if local_radius < 0.0001:
-                local_radius = 0.0001 # HACK: improve normal creation
-            for xz_iteration in range(0, resolution):
-                xz_value = 2 * math.pi * xz_iteration / resolution
-                x = local_radius * math.cos(xz_value)
-                z = local_radius * math.sin(xz_value)
-                verts.append([x, y, z])
-
-        mesh.setVertices(numpy.asarray(verts, dtype=numpy.float32))
+    def _toMeshData(self, tri_node: trimesh.base.Trimesh) -> MeshData:
+        tri_faces = tri_node.faces
+        tri_vertices = tri_node.vertices
 
         indices = []
-        start_row = 0
+        vertices = []
 
-        for z_iteration in range(0, resolution - 1):
-            for xz_iteration in range(0, resolution):
-                start_face = start_row + xz_iteration
-                if xz_iteration != resolution - 1:
-                    indices.append([start_face + 0, start_face + 1, start_face + resolution])
-                    indices.append([start_face + 1, start_face + resolution + 1, start_face + resolution])
-                else:
-                    indices.append([start_face + 0, start_row, start_face + resolution])
-                    indices.append([start_row, start_row + resolution, start_face + resolution])
-            start_row = start_row + resolution
+        index_count = 0
+        face_count = 0
+        for tri_face in tri_faces:
+            face = []
+            for tri_index in tri_face:
+                vertices.append(tri_vertices[tri_index])
+                face.append(index_count)
+                index_count += 1
+            indices.append(face)
+            face_count += 1
 
-        mesh.setIndices(numpy.asarray(indices, dtype=numpy.int32))
+        vertices = numpy.asarray(vertices, dtype=numpy.float32)
+        indices = numpy.asarray(indices, dtype=numpy.int32)
+        normals = calculateNormalsFromIndexedVertices(vertices, indices, face_count)
 
-        mesh.calculateNormals()
-
-        self._addShape(mesh.build())
+        mesh_data = MeshData(vertices=vertices, indices=indices, normals=normals)
+        return mesh_data
 
     def _addShape(self, meshData):
         node = CuraSceneNode()
