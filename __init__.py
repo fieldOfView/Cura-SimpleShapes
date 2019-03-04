1f36df1b16d7ae8872cb10fa958d490b18588b10
fieldOfView <aldo@fieldofview.com>
2019-03-04 12:21:47 +0100

    Update for Cura 3.5 and newer

--- __init__.py
+++ __init__.py
@@ -1,10 +1,7 @@
-# Copyright (c) 2015 Ultimaker B.V.
-# Cura is released under the terms of the LGPLv3 or higher.
+# Copyright (c) 2018 fieldOfView
+# The SimpleShapes plugin is released under the terms of the AGPLv3 or higher.
 
 from . import SimpleShapes
-
-from UM.i18n import i18nCatalog
-catalog = i18nCatalog("cura")
 
 def getMetaData():
     return {}
