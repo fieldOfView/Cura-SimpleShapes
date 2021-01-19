4c614fb60ab54dadb8374630c571aa3477a57b99
5axes <5axes@users.noreply.github.com>
2021-01-19 10:24:06 +0100

    Add Possibility to define Size

--- CalibrationShapes.py
+++ CalibrationShapes.py
@@ -1,19 +1,26 @@
 #-----------------------------------------------------------------------------------
-# PArt of the initial source code for primitive Shape (c) 2018 fieldOfView
+# Part of the initial source code for primitive Shape (c) 2018 fieldOfView
 # The CalibrationShapes plugin is released under the terms of the AGPLv3 or higher.
 # Modifications 5@xes 2020-2021
 #-----------------------------------------------------------------------------------
+from PyQt5.QtCore import QObject, pyqtProperty, pyqtSignal, pyqtSlot, QUrl
 
-from PyQt5.QtCore import QObject
-
+# Imports from the python standard library to build the plugin functionality
+from code import InteractiveInterpreter
 import os
 import re
 import numpy
 import trimesh
 import shutil
 from shutil import copyfile
+import platform
+import sys
+from datetime import datetime
+
 
 from UM.Extension import Extension
+from UM.PluginRegistry import PluginRegistry
+from UM.Application import Application
 from cura.CuraApplication import CuraApplication
 
 from UM.Mesh.MeshData import MeshData, calculateNormalsFromIndexedVertices
@@ -30,12 +37,38 @@ from UM.Message import Message
 from UM.i18n import i18nCatalog
 catalog = i18nCatalog("cura")
 
-class CalibrationShapes(Extension, QObject,):
-    __size = 20
+#global variables
+#On some windows systems cura has no permission to use the plugin directory for files
+#us appdata on windows instead
+if platform.system() == "Windows":
+    dir_path = os.path.join(os.getenv('APPDATA'),"cura")
+else:
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+
+#This class is the extension and doubles as QObject to manage the qml    
+class CalibrationShapes(QObject, Extension, InteractiveInterpreter):
+    #Create an api
+    from cura.CuraApplication import CuraApplication
+    api = CuraApplication.getInstance().getCuraAPI()
+
+    # The QT signal, which signals an update for user information text
+    userInfoTextChanged = pyqtSignal()
     
     def __init__(self, parent = None) -> None:
         QObject.__init__(self, parent)
         Extension.__init__(self)
+        InteractiveInterpreter.__init__(self, globals())
+
+        #Inzialize varables
+        self.userText = ""
+        self._continueDialog = None
+        
+        # set the preferences to store the default value
+        self._preferences = CuraApplication.getInstance().getPreferences()
+        self._preferences.addPreference("calibrationshapes/size", 20)
+        
+        # convert as float to avoid further issue
+        self._size = float(self._preferences.getValue("calibrationshapes/size"))
         
         # Suggested solution from fieldOfView . Unfortunatly it doesn't works 
         # https://github.com/5axes/Calibration-Shapes/issues/1
@@ -44,7 +77,8 @@ class CalibrationShapes(Extension, QObject,):
 
         self._controller = CuraApplication.getInstance().getController()
         self._message = None
-
+        
+        self.setMenuName(catalog.i18nc("@item:inmenu", "Part for calibration"))
         self.addMenuItem(catalog.i18nc("@item:inmenu", "Add a cube"), self.addCube)
         self.addMenuItem(catalog.i18nc("@item:inmenu", "Add a cylinder"), self.addCylinder)
         self.addMenuItem(catalog.i18nc("@item:inmenu", "Add a sphere"), self.addSphere)
@@ -59,7 +93,137 @@ class CalibrationShapes(Extension, QObject,):
         self.addMenuItem(catalog.i18nc("@item:inmenu", "Add an Overhang Test"), self.addOverhangTest)
         self.addMenuItem(" ", lambda: None)
         self.addMenuItem(catalog.i18nc("@item:inmenu", "Copy Scripts"), self.copyScript)
+        self.addMenuItem(catalog.i18nc("@item:inmenu", "Define default size"), self.defaultSize)
+  
+        #Inizialise debug-log
+        self.createLog()
+        self.writeToLog("created log!")
+        self.writeToLog("OS: "+platform.system())
+
+        #Inzialize varables
+        self.userText = ""
+        self._continueDialog = None
+        # here the height from which should be continued is saved
+        self._printFromHeight = None
+        
+    # Copy the scripts to the right directory ( Temporaray solution)
+    def defaultSize(self) -> None:
     
+        if self._continueDialog is None:
+            self._continueDialog = self._createDialogue()
+        self._continueDialog.show()
+        
+ 
+    #====User Input=====================================================================================================
+
+    #The QT property, which is computed on demand from our userInfoText when the appropriate signal is emitted
+    @pyqtProperty(str, notify= userInfoTextChanged)
+    def userInfoText(self):
+        return self.userText
+
+    #This method builds the dialog from the qml file and registers this class
+    #as the manager variable
+    def _createDialogue(self):
+        qml_file_path = os.path.join(PluginRegistry.getInstance().getPluginPath(self.getPluginId()), "CalibrationShapes.qml")
+        component_with_context = Application.getInstance().createQmlComponent(qml_file_path, {"manager": self})
+        return component_with_context
+
+    #is called when a key gets released in the height inputField (twice for some reason)
+    @pyqtSlot(str)
+    def heightEntered(self, text):
+        #Is the textfield empty? Don't show a message then
+        if text =="":
+            #self.writeToLog("Height-Textfield: Empty")
+            self.userMessage("", "ok")
+            return
+
+        #Convert commas to points [+1 User Experience!]
+        text = text.replace(",",".")
+
+        #self.writeToLog("Height-Textfield: read value "+text)
+
+        #is the entered Text a number?
+        try:
+            float(text)
+        except ValueError:
+            self.userMessage("Entered size invalid: " + text,"bad")
+            self.handleCommand(text)
+            return
+        self._size = float(text)
+
+        #Check if positive
+        if self._size <= 0:
+            self.userMessage("Size value must be positive !","bad")
+            self._size = 20
+            return
+
+        self.writeToLog("Set calibrationshapes/size printFromHeight to: " + text)
+        self._preferences.setValue("calibrationshapes/size", self._size)
+        
+        #clear the message Field
+        self.userMessage("", "ok")
+
+
+    #Gets called when the circular <?>-Button is clicked
+    #Some users we tested with, tried clicking the ? which would prevent the tooltip from showing So we tell them
+    @pyqtSlot()
+    def buttonHelpPressed(self):
+        self.writeToLog("---ButtonHelp pressed")
+ 
+    #=====Text Output===================================================================================================
+
+    #creates a new log file, and deletes the old one.
+    def createLog(self):
+        file = open(os.path.join(dir_path, "CalibrationShapes.txt"), "w")
+        self.writeToLog("Creating Log in: " + str(dir_path))
+
+    #writes the message to the log, includes timestamp, length is fixed
+    def writeToLog(self, str):
+        file = open(os.path.join(dir_path, "CalibrationShapes.txt"), "a")
+        now = datetime.now()
+        current_time = now.strftime("%H:%M:%S")
+        file.write("["+current_time+"] ")
+        #clip length
+        if len(str) > 200:
+            str = "+++To Long+++ "+ str[0:50]+"..."
+        file.write(str)
+        file.write("\n")
+
+    #handles commands entered into the size textfield
+    def handleCommand(self,text):
+        #the command log opens the log file and shows its directory-path
+        if text == "log":
+            os.system("start " + os.path.join(dir_path, "CalibrationShapes.txt"))
+            self.userMessage(str(dir_path),"ok")
+            self.writeToLog("Command: log")
+        #the command os shows the os name
+        if text == "os":
+            self.writeToLog("Command: os")
+            self.userMessage(platform.system(), "ok")
+        #the command dir lists all commands (yes, all three of them)
+        if text == "dir":
+            self.writeToLog("Command: dir")
+            self.userMessage("Commands: log, os, dir", "ok")
+
+    #Sends a user message to the Info Textfield, color depends on status (prioritized feedback)
+    #Bad for Errors and Warnings
+    #Grey for details and messages that aren't interesting for advanced users
+    #Cura-Blue for successfully completing a task
+    def userMessage(self, message, status):
+        if status is "good":
+            self.userText = "<font color='#151354'>" + message + "</font>"
+        else:
+            if status is "bad":
+                self.userText = "<font color='#a00000'>" + message + "</font>"
+            else:
+                if status is "ok":
+                    self.userText = "<font color='#9fa4b0'>" + message + "</font>"
+                else:
+                    self.writeToLog("Error: Invalid status: "+status)
+                    return
+        self.writeToLog("User Message: "+message)
+        self.userInfoTextChanged.emit()
+ 
     # Copy the scripts to the right directory ( Temporaray solution)
     def copyScript(self) -> None:
         File_List = ['RetractTower.py', 'SpeedTower.py', 'TempFanTower.py']
@@ -87,7 +251,7 @@ class CalibrationShapes(Extension, QObject,):
         self._message.show()
     
     def addCube(self) -> None:
-        self._addShape(self._toMeshData(trimesh.creation.box(extents = [self.__size, self.__size, self.__size])))
+        self._addShape(self._toMeshData(trimesh.creation.box(extents = [self._size, self._size, self._size])))
 
     def addCalibrationCube(self) -> None:
         model_definition_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "models\CalibrationCube.stl")
@@ -119,18 +283,18 @@ class CalibrationShapes(Extension, QObject,):
  
     def addCylinder(self) -> None:
         Rx = trimesh.transformations.rotation_matrix(math.radians(90), [1, 0, 0])
-        self._addShape(self._toMeshData(trimesh.creation.cylinder(radius = self.__size / 2, height = self.__size, sections=90, transform = Rx )))      
+        self._addShape(self._toMeshData(trimesh.creation.cylinder(radius = self._size / 2, height = self._size, sections=90, transform = Rx )))      
 
     def addTube(self) -> None:
         #origin, xaxis, yaxis, zaxis = [0, 0, 0], [1, 0, 0], [0, 1, 0], [0, 0, 1]
         # S = trimesh.transformations.scale_matrix(20, origin)
         xaxis = [1, 0, 0]
         Rx = trimesh.transformations.rotation_matrix(math.radians(90), xaxis)
-        self._addShape(self._toMeshData(trimesh.creation.annulus(r_min = self.__size / 4, r_max = self.__size / 2, height = self.__size, sections = 90, transform = Rx )))
+        self._addShape(self._toMeshData(trimesh.creation.annulus(r_min = self._size / 4, r_max = self._size / 2, height = self._size, sections = 90, transform = Rx )))
         
     def addSphere(self) -> None:
         # subdivisions (int) – How many times to subdivide the mesh. Note that the number of faces will grow as function of 4 ** subdivisions, so you probably want to keep this under ~5
-        self._addShape(self._toMeshData(trimesh.creation.icosphere(subdivisions=4,radius = self.__size / 2)))
+        self._addShape(self._toMeshData(trimesh.creation.icosphere(subdivisions=4,radius = self._size / 2)))
     
     # Intial Source code from  fieldOfView
     def _toMeshData(self, tri_node: trimesh.base.Trimesh) -> MeshData:
---
+++ CalibrationShapes.qml
@@ -0,0 +1,210 @@
+// Import the standard GUI elements from QTQuick
+import QtQuick 2.2
+import QtQuick.Controls 1.1
+import QtQuick.Controls.Styles 1.1
+import QtQuick.Layouts 1.1
+import QtQuick.Dialogs 1.1
+import QtQuick.Window 2.2
+
+// Import the Uranium GUI elements, which are themed for Cura
+import UM 1.1 as UM
+import Cura 1.0 as Cura
+
+// Dialog
+Window
+{
+    id: base
+
+    title: "Calibration Shapes"
+
+    color: "#fafafa" //Background color of cura: #fafafa
+
+    // We don't want the dialog to block input in the main window
+    modality: Qt.NonModal
+
+    //We want the window to stay on top
+    flags: Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint | Qt.WindowCloseButtonHint
+
+    // Setting the dimensions of the dialog window and prohibiting resizing
+    width: 300
+    height: 50
+    minimumWidth: 350
+    minimumHeight: 50
+
+    //Position the window
+    x: Screen.width*0.5 - width - 50
+	// 400
+    y: 400 
+
+    //This is for giving the Window a boarder and a background color
+    Rectangle {
+        id: bg_rect
+        width: 300
+        height: 50
+        color: "#fff"
+		// "#ededed"
+        border.color: "#D22"
+        border.width: 3
+        radius: 5
+    }
+
+
+    // Connecting our variable to the computed property of the manager
+    property string userInfoText: manager.userInfoText
+
+    //This Button is for closing the dialog
+    Button
+    {
+        id: close_button
+        text: "<font color='#ffffff'>" + "x" + "</font>"
+        width: 25
+        height: 25
+
+        anchors.top: parent.top
+        anchors.topMargin: 10
+        anchors.right: parent.right
+        anchors.rightMargin: 10
+
+        tooltip: "close this dialog"
+
+        style: ButtonStyle{
+            background: Rectangle {
+                implicitWidth: 100
+                implicitHeight: 25
+                radius: 3
+                color: "#D22"
+            }
+        }
+
+        onClicked:
+        {
+            base.close();
+        }
+
+    }
+
+
+    //Text "Size: "
+    Text
+    {
+        id: test_size
+        text: "Size:"
+        font.family: "Helvetica"
+        font.pointSize: 14
+        color: "#131151"
+
+        anchors.top: close_button.top
+        anchors.topMargin: 10
+        anchors.left: parent.left
+        anchors.leftMargin: 10
+    }
+
+    //User input of height
+    TextField
+    {
+        id: height_input
+        width: 80
+        text: ""
+        placeholderText: "ie. 20.0"
+
+        anchors.bottom: test_size.bottom
+        anchors.bottomMargin: 0
+        anchors.left: test_size.right
+        anchors.leftMargin: 10
+
+        font.pointSize: 11
+
+        //The window musn't close when enter is pressed
+        Keys.onReturnPressed:
+        {
+            event.accepted = true
+        }
+
+        //Return the new entered value
+        Keys.onReleased:
+        {
+            manager.heightEntered(height_input.text)
+        }
+    }
+
+    //Text: "mm"
+    Text
+    {
+        id: text_unit
+        text: "mm"
+        font.family: "Arial"
+        font.pointSize: 14
+        color: "#131151"
+
+        anchors.bottom: test_size.bottom
+        anchors.bottomMargin: 0
+        anchors.left: height_input.right
+        anchors.leftMargin: 5
+    }
+
+    //This Button only exists to hold the help Tooltip
+    Button
+    {
+        id: help_button
+        text: "<font color='#888'>" + "?" + "</font>"
+        //text: "?"
+        width: 21
+        height: 21
+
+        anchors.bottom: close_button.bottom
+        anchors.bottomMargin: 2
+        anchors.right: close_button.left
+        anchors.rightMargin: 5
+
+        tooltip:
+        "This plugin offer you the possibility to define a standard sized element.
+        Define in the Size field the default size for the element"
+
+        onClicked:
+        {
+            //do button action
+            manager.buttonHelpPressed()
+        }
+
+        style: ButtonStyle{
+            background: Rectangle {
+                implicitWidth: 15
+                implicitHeight: 15
+                radius: 10
+                color: "#fafafa"
+                border.width: 1
+                border.color: "#888"
+            }
+        }
+
+    }
+
+
+
+
+    //Textfield for User Messages
+    Text
+    {
+
+        id: user_text
+
+        width: 280
+        anchors.top: button_continue.bottom
+        anchors.topMargin: 10
+        anchors.left: parent.left
+        anchors.leftMargin: 10
+
+        //text: "Info"
+        text: userInfoText
+
+        font.family: "Arial"
+        font.pointSize: 11
+        //The color gets overwritten by the html tags added to the text
+        color: "black"
+
+        wrapMode: Text.Wrap
+
+
+    }
+
+}
