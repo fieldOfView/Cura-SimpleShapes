c814bf704c74f2d519b837862aa423d404b593f2
5axes <5axes@users.noreply.github.com>
2022-02-13 23:57:06 +0100

    Update GCodeDocumentation.py

--- resources/scripts/GCodeDocumentation.py
+++ resources/scripts/GCodeDocumentation.py
@@ -1,11 +1,11 @@
-# Copyright (c) 2019 5axes
+# Copyright (c) 2019-2022 5axes
 #
 # Description:  This Script add Parameters description in the Header of the G-Code file...
 #               Please look at the option
 #               - Advanced_desc : Advanced description
 #               - extruder_nb   : Specify the extruder nb information to output in case of multi-extruder 
 #                                   you must use as many script as you have some exturder on your machine
-#               - Position      : Position of the Texte in the ISo File : 1 = First line , 2 second line etc ...
+#               - Position      : Position of the Text in the GCode File : 1 = First line , 2 second line etc ...
 #
 # Version 1.01 of 20/10/2019 Who  : 5axes What : Addition of the names of the parameters with the label
 # Version 1.02 of 21/10/2019 Who  : 5axes What : Addition of new parameters
@@ -26,6 +26,7 @@
 # Version 5.03 of 07/04/2020 Who  : 5axes What : Add line width info
 # Version 5.04 of 07/05/2020 Who  : 5axes What : Adding support flow info and info on xy_offset (Version 4.6) 
 # Version 5.1.0 of 09/05/2020 Who : 5axes What : Add message for 4.6
+# Version 5.2.0 of 13/02/2022 Who : 5axes What : New Settings
 #
 #
 import string
@@ -39,7 +40,7 @@ catalog = i18nCatalog("cura")
         
 ## 
 class GCodeDocumentation(Script):
-    version = "5.0.4"
+    version = "5.2.0"
     
     def getSettingDataString(self):
         return """{
@@ -122,23 +123,30 @@ class GCodeDocumentation(Script):
         adv_desc = self.getSettingValueByKey("advanced_desc")
         extruder_id  = self.getSettingValueByKey("extruder_nb")
         extruder_id = extruder_id -1
+        extrud = Application.getInstance().getGlobalContainerStack().extruderList
         _msg = ''
         VersC=1.0
 
-        # Test version car parametre seulement a partir de 4.6
+
+        # Test version for futur release 4.9
         if "master" in CuraVersion or "beta" in CuraVersion or "BETA" in CuraVersion:
-            Logger.log('d', "Info G-Code Documentation --> " + str(CuraVersion))
-            VersC=4.6  # Master is always a developement version.
+            # Master is always a developement version.
+            Major=4
+            Minor=9
+
         else:
             try:
-                VersC = int(CuraVersion.split(".")[0])+int(CuraVersion.split(".")[1])/10
+                Major = int(CuraVersion.split(".")[0])
+                Minor = int(CuraVersion.split(".")[1])
             except:
                 pass
         
+        Logger.log('d', "Info G-Code Documentation --> " + str(Major) + " / " + str(Minor))
+        
+        
         # test depuis 3.6
-        if VersC < 3.6:
+        if Major < 4 or Minor < 6 :
             _msg = "Attention Version Cura " + str(CuraVersion)
-            Logger.log('d', "Info G-Code Documentation --> " + str(VersC))
         
         if _msg != None and _msg != '':
             Message("Info G-Code Documentation :" + "\n" + _msg, title = catalog.i18nc("@info:title", "Post Processing")).show()
@@ -208,16 +216,23 @@ class GCodeDocumentation(Script):
             replace_string = replace_string + self.GetDataExtruder(extruder_id,"wall_line_count",10)
         #   wall_0_wipe_dist
         if adv_desc :
-            replace_string = replace_string + self.GetDataExtruder(extruder_id,"wall_0_wipe_dist")           
-        #   top_layers
+            replace_string = replace_string + self.GetDataExtruder(extruder_id,"wall_0_wipe_dist")  
+        #   xy_offset
         if adv_desc :
-            replace_string = replace_string + self.GetDataExtruder(extruder_id,"top_layers",10)
-        #   bottom_layers
+            replace_string = replace_string + self.GetDataExtruder(extruder_id,"xy_offset")   
+        #   xy_offset_layer_0 
         if adv_desc :
-            replace_string = replace_string + self.GetDataExtruder(extruder_id,"bottom_layers",10)
-        #   top_bottom_pattern 
+            replace_string = replace_string + self.GetDataExtruder(extruder_id,"xy_offset_layer_0")  
+        #   hole_xy_offset 
         if adv_desc :
-            replace_string = replace_string + self.GetDataExtruder(extruder_id,"top_bottom_pattern")
+            if Major > 4 or ( Major == 4 and Minor >= 6 ) :
+                replace_string = replace_string + self.GetDataExtruder(extruder_id,"hole_xy_offset")  
+        #   z_seam_type 
+        if adv_desc :
+            replace_string = replace_string + self.GetDataExtruder(extruder_id,"z_seam_type")
+        #   z_seam_corner 
+        if adv_desc :
+            replace_string = replace_string + self.GetDataExtruder(extruder_id,"z_seam_corner")  
         #   outer_inset_first
         if adv_desc :
             replace_string = replace_string + self.GetDataExtruder(extruder_id,"outer_inset_first")
@@ -230,23 +245,37 @@ class GCodeDocumentation(Script):
         #   fill_outline_gaps
         if adv_desc :
             replace_string = replace_string + self.GetDataExtruder(extruder_id,"fill_outline_gaps")
-        #   xy_offset
+
+        #   -----------------------------------  top_bottom -----------------------------
+        if Major > 4 or ( Major == 4 and Minor >= 9 ) :
+            GetLabel = Application.getInstance().getGlobalContainerStack().getProperty("top_bottom", "label") 
+            if adv_desc :
+                replace_string = replace_string + self.SetSect(GetLabel)
+            
+        #   top_layers
         if adv_desc :
-            replace_string = replace_string + self.GetDataExtruder(extruder_id,"xy_offset")   
-        #   xy_offset_layer_0 
+            replace_string = replace_string + self.GetDataExtruder(extruder_id,"top_layers",10)
+        #   bottom_layers
         if adv_desc :
-            replace_string = replace_string + self.GetDataExtruder(extruder_id,"xy_offset_layer_0")  
-        #   hole_xy_offset 
-        if adv_desc and VersC >= 4.6:
-            replace_string = replace_string + self.GetDataExtruder(extruder_id,"hole_xy_offset")  
-        #   z_seam_type 
+            replace_string = replace_string + self.GetDataExtruder(extruder_id,"bottom_layers",10)
+        #   top_bottom_pattern 
         if adv_desc :
-            replace_string = replace_string + self.GetDataExtruder(extruder_id,"z_seam_type")
-        #   z_seam_corner 
+            replace_string = replace_string + self.GetDataExtruder(extruder_id,"top_bottom_pattern")    
+        #   skin_monotonic
         if adv_desc :
-            replace_string = replace_string + self.GetDataExtruder(extruder_id,"z_seam_corner")            
+            if Major > 4 or ( Major == 4 and Minor >= 9 ) :
+                replace_string = replace_string + self.GetDataExtruder(extruder_id,"skin_monotonic")
         #   ironing_enabled
         replace_string = replace_string + self.GetDataExtruder(extruder_id,"ironing_enabled")
+        ironing_enabled = bool(extrud[extruder_id].getProperty("ironing_enabled", "value"))
+        
+        if adv_desc and ironing_enabled :
+            replace_string = replace_string + self.GetDataExtruder(extruder_id,"ironing_pattern") 
+            replace_string = replace_string + self.GetDataExtruder(extruder_id,"ironing_only_highest_layer") 
+       
+        if adv_desc :
+            if Major > 4 or ( Major == 4 and Minor >= 9 ) :
+                replace_string = replace_string + self.GetDataExtruder(extruder_id,"ironing_monotonic")
         
         #   -----------------------------------  infill ------------------------------ 
         GetLabel = Application.getInstance().getGlobalContainerStack().getProperty("infill", "label")
@@ -408,6 +437,32 @@ class GCodeDocumentation(Script):
             
         #   support_enable 
         replace_string = replace_string + self.GetDataExtruder(extruder_id,"support_enable")
+        
+        #   support_structure
+        support_structure=Application.getInstance().getGlobalContainerStack().getProperty("support_structure", "value")
+        Logger.log('d', "Info G-Code Documentation support_structure --> " + str(support_structure))
+        
+        if Major > 4 or ( Major == 4 and Minor >= 8 ) :
+            replace_string = replace_string + self.GetDataExtruder(extruder_id,"support_structure")
+ 
+        #   support_tree_enable 
+        if Major < 4 or ( Major == 4 and Minor <= 9 ) : 
+            replace_string = replace_string + self.GetDataExtruder(extruder_id,"support_tree_enable")
+        
+        if support_structure == "tree" :       
+            #   support_tree_angle
+            if adv_desc :
+                replace_string = replace_string + self.GetDataExtruder(extruder_id,"support_tree_angle",5)
+            #   support_tree_branch_distance
+            if adv_desc :
+                replace_string = replace_string + self.GetDataExtruder(extruder_id,"support_tree_branch_distance",5)
+            #   support_tree_branch_diameter
+            if adv_desc :
+                replace_string = replace_string + self.GetDataExtruder(extruder_id,"support_tree_branch_diameter",5)
+            #   support_tree_branch_diameter_angle
+            if adv_desc :
+                replace_string = replace_string + self.GetDataExtruder(extruder_id,"support_tree_branch_diameter_angle",5) 
+            
         #   support_type
         replace_string = replace_string + self.GetDataExtruder(extruder_id,"support_type")
         #   support_angle
@@ -421,6 +476,13 @@ class GCodeDocumentation(Script):
         #   support_infill_rate
         if adv_desc :
             replace_string = replace_string + self.GetDataExtruder(extruder_id,"support_infill_rate")
+        #   support_wall_count
+        #   support_brim_enable
+        if adv_desc :
+            if Major > 4 or ( Major == 4 and Minor >= 8 ) : 
+                replace_string = replace_string + self.GetDataExtruder(extruder_id,"support_wall_count")
+                replace_string = replace_string + self.GetDataExtruder(extruder_id,"support_brim_enable")
+                
         #   support_z_distance
         if adv_desc :
             replace_string = replace_string + self.GetDataExtruder(extruder_id,"support_z_distance",5)
@@ -451,7 +513,7 @@ class GCodeDocumentation(Script):
         #   support_interface_pattern
         if adv_desc :
             replace_string = replace_string + self.GetDataExtruder(extruder_id,"support_interface_pattern")
-            
+                             
 
         #   -----------------------------------  platform_adhesion ----------------------------- 
         GetLabel = Application.getInstance().getGlobalContainerStack().getProperty("platform_adhesion", "label")
@@ -459,11 +521,27 @@ class GCodeDocumentation(Script):
             replace_string = replace_string + self.SetSect(GetLabel)
             
         #   adhesion_type
+        adhesion_type = Application.getInstance().getGlobalContainerStack().getProperty("adhesion_type", "value")
         replace_string = replace_string + self.GetDataExtruder(extruder_id,"adhesion_type")
         #   brim_width 
         if adv_desc :
-            replace_string = replace_string + self.GetDataExtruder(extruder_id,"brim_width")
-
+            if adhesion_type == "brim" :
+                replace_string = replace_string + self.GetDataExtruder(extruder_id,"brim_width")
+        if adv_desc :
+            if adhesion_type == "raft" :
+                replace_string = replace_string + self.GetDataExtruder(extruder_id,"raft_surface_layers")
+                replace_string = replace_string + self.GetDataExtruder(extruder_id,"raft_base_thickness",3)
+                replace_string = replace_string + self.GetDataExtruder(extruder_id,"raft_base_line_width",3)
+                
+        #   skirt_gap / brim_gap
+        if adv_desc :
+            if Major > 4 or ( Major == 4 and Minor >= 8 ) :
+                if adhesion_type == "skirt" :
+                    replace_string = replace_string + self.GetDataExtruder(extruder_id,"skirt_gap")
+                if adhesion_type == "brim" :
+                    replace_string = replace_string + self.GetDataExtruder(extruder_id,"brim_gap")
+                    replace_string = replace_string + self.GetDataExtruder(extruder_id,"brim_outside_only")
+                    
         #   -----------------------------------  dual -----------------------------
         if extruder_count>0:
             GetLabel = Application.getInstance().getGlobalContainerStack().getProperty("dual", "label")
@@ -520,21 +598,18 @@ class GCodeDocumentation(Script):
         GetLabel = Application.getInstance().getGlobalContainerStack().getProperty("experimental", "label")
         if adv_desc :
             replace_string = replace_string + self.SetSect(GetLabel)
-            
+ 
         #   support_tree_enable 
-        replace_string = replace_string + self.GetDataExtruder(extruder_id,"support_tree_enable")
-        #   support_tree_angle
+        if Major < 4 or ( Major == 4 and Minor <= 9 ) : 
+            replace_string = replace_string + self.GetDataExtruder(extruder_id,"support_tree_enable")
+            
+         #   skin_monotonic
         if adv_desc :
-            replace_string = replace_string + self.GetDataExtruder(extruder_id,"support_tree_angle",5)
-        #   support_tree_branch_distance
-        if adv_desc :
-            replace_string = replace_string + self.GetDataExtruder(extruder_id,"support_tree_branch_distance",5)
-        #   support_tree_branch_diameter
-        if adv_desc :
-            replace_string = replace_string + self.GetDataExtruder(extruder_id,"support_tree_branch_diameter",5)
-        #   support_tree_branch_diameter_angle
-        if adv_desc :
-            replace_string = replace_string + self.GetDataExtruder(extruder_id,"support_tree_branch_diameter_angle",5)
+            if Major > 4 or ( Major == 4 and Minor >= 9 ) :
+                skin_monotonic = bool(extrud[extruder_id].getProperty("skin_monotonic", "value"))
+                if skin_monotonic :
+                    replace_string = replace_string + self.GetDataExtruder(extruder_id,"roofing_monotonic")
+        
         #   coasting_enable
         if adv_desc :
             replace_string = replace_string + self.GetDataExtruder(extruder_id,"coasting_enable")
